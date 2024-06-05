698672bab5729eeec8992115f946fadac3212674
Benoit Cote <bcote@anl.gov>
2024-06-05 13:57:49 -0500

    Added Gunicorn files to run service with sudo systemctl

---
+++ gunicorn.config.py
@@ -0,0 +1,18 @@
+"""gunicorn WSGI server configuration."""
+
+bind = '0.0.0.0:7000'
+max_requests = 100
+workers = 4
+
+# Access log - records incoming requests
+accesslog = "/var/log/inference-service/backend_gateway.access.log"
+
+# Error log - records Gunicorn server goings-on
+errorlog = "/var/log/inference-service/backend_gateway.error.log"
+
+# Whether to send Django output to the error log
+capture_output = True
+
+# How verbose the Gunicorn error logs should be
+loglevel = "debug"
+enable_stdio_inheritance = True
