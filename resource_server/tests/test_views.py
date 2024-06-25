d1261991b517dc0d789ba27bbb1d88e676d2dd73
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2024-06-25 15:36:29 -0700

    started tests for Polaris POST requests

--- resource_server/tests/test_views.py
+++ resource_server/tests/test_views.py
@@ -22,6 +22,10 @@ class ResourceServerViewTestCase(APITestCase):
         # Fill Django test database
         call_command("loaddata", "fixtures/endpoints.json")
 
+        # Extract all endpoint objects from the database
+        self.db_endpoints = Endpoint.objects.all()
+        self.len_db_endpoints = len(self.db_endpoints)
+
         # Create mock access tokens
         self.active_token = mock_utils.get_mock_access_token(active=True, expired=False)
         self.expired_token = mock_utils.get_mock_access_token(active=True, expired=True)
@@ -46,7 +50,7 @@ class ResourceServerViewTestCase(APITestCase):
         # Make sure non-GET requests are not allowed
         for method in [self.factory.post, self.factory.put, self.factory.delete]:
             response = view(method(url))
-            self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)    
+            self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
         # Make sure GET requests succeed when providing a valid access token
         headers = mock_utils.get_mock_headers(access_token=self.active_token, bearer=True)
@@ -54,20 +58,42 @@ class ResourceServerViewTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # Make sure GET requests return the correct number of endpoints
-        db_endpoints = Endpoint.objects.all()
-        self.assertEqual(len(db_endpoints), len(response.data))
+        self.assertEqual(self.len_db_endpoints, len(response.data))
 
         # For each endpoint in the Django database ...
-        for endpoint in db_endpoints:
+        for endpoint in self.db_endpoints:
 
             # Build dictionary entry to compare with the request response
             entry = {
-                "endpoint_url": f"/resource_server/{endpoint.cluster}/{endpoint.framework}/completions/",
+                "endpoint_url": self.__get_endpoint_url(endpoint),
                 "model_name": endpoint.model
             }
 
             # Make sure the entry is in the the request response
             self.assertIn(entry, response.data)
+
+    
+    # Test Polaris (POST)
+    def test_polaris_view(self):
+
+        # Select the targeted Django view
+        view = views.Polaris.as_view()
+
+        # For each endpoint ...
+        for endpoint in self.db_endpoints:
+            
+            # Build the targeted Django URL
+            url = self.__get_endpoint_url(endpoint)
+
+            # Make sure GET requests fail if something is wrong with the authentication
+            self.__verify_headers_failures(url=url, view=view, method=self.factory.post)
+
+            # Make sure non-POST requests are not allowed
+            for method in [self.factory.get, self.factory.put, self.factory.delete]:
+                response = view(method(url))
+                self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+            #TODO: Make more tests here
 
 
     # Verify headers failures
@@ -93,3 +119,7 @@ class ResourceServerViewTestCase(APITestCase):
         response = view(method(url, headers=headers))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         
+
+    # Get endpoint URL
+    def __get_endpoint_url(self, endpoint):
+        return f"/resource_server/{endpoint.cluster}/{endpoint.framework}/completions/"
