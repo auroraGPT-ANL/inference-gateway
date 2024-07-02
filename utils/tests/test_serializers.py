444f426900fc705713140affe7896d5b2ef92b36
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2024-07-02 11:32:30 -0700

    added tests for chat openai serializer

--- utils/tests/test_serializers.py
+++ utils/tests/test_serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework.exceptions import ValidationError
-from utils.serializers import OpenAILegacyParamSerializer
+from utils.serializers import OpenAILegacyParamSerializer, OpenAIParamSerializer
 import json
 
 # Test utils/serializers.py
@@ -19,10 +19,14 @@ class UtilsSerializersTestCase(APITestCase):
         # Load valid OpenAI model parameters
         with open(f"{base_path}/valid_legacy_openai.json") as json_file:
             self.valid_legacy_openai_params = json.load(json_file)
+        with open(f"{base_path}/valid_chat_openai.json") as json_file:
+            self.valid_chat_openai_params = json.load(json_file)
 
         # Load invalid OpenAI model parameters
         with open(f"{base_path}/invalid_legacy_openai.json") as json_file:
             self.invalid_legacy_openai_params = json.load(json_file)
+        with open(f"{base_path}/invalid_chat_openai.json") as json_file:
+            self.invalid_chat_openai_params = json.load(json_file)
 
 
     # Test OpenAILegacyParamSerializer for validation
@@ -46,3 +50,24 @@ class UtilsSerializersTestCase(APITestCase):
                 print(invalid_params) # TODO: Is there a better way to print out info with self.assertFalse?
             self.assertFalse(serializer.is_valid(raise_exception=False))
     
+
+    # Test OpenAIParamSerializer for validation
+    def test_OpenAIParamSerializer_validation(self):
+
+        # For each valid set of parameters ...
+        for valid_params in self.valid_chat_openai_params:
+
+            # Send the data to the serializer and make sure the data is valid
+            serializer = OpenAIParamSerializer(data=valid_params)
+            self.assertTrue(serializer.is_valid(raise_exception=True))
+
+        # For each invalid set of parameters ...
+        for invalid_params in self.invalid_chat_openai_params:
+
+            # Send the data to the serializer
+            serializer = OpenAIParamSerializer(data=invalid_params)
+
+            # Make sure the data is not valid
+            if serializer.is_valid(raise_exception=False):
+                print(invalid_params) # TODO: Is there a better way to print out info with self.assertFalse?
+            self.assertFalse(serializer.is_valid(raise_exception=False))
