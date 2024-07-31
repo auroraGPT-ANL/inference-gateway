dbb9891a78478bf071bc030be8013b86551464df
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2024-07-31 07:16:39 -0700

    Increased max_requests and nb of workers

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -7,13 +7,21 @@ bind = '0.0.0.0:7000'
 timeout = 1800
 
 # Number of requests before workers automatically restart
-# This helps limit damage caused by memory leaks
-max_requests = 100
+# This helps limit damage caused by memory leaks (ensure workers release memory from time to time)
+max_requests = 3000
+
+# Random number (between 0 and max_requests_jitter) added to the max_requests number
+# This randomize when workers are restarting and limit the risk of all workers restarting at the same time
+max_requests_jitter = 300
+
+# Maximum number of pending connections (default 2048)
+# If the number of pending requests exceed this number, there will be a ConnectTimeout/MaxEntry error
+backlog = 2048
 
 # Process only one request at a time to avoid stealing Globus App SDK client session
 # This is temporary, once we use user's credentials and share compute endpoints, we can scale up
 worker_class = "sync"
-workers = 4
+workers = 9
 threads = 1
 
 # Access and error logs
