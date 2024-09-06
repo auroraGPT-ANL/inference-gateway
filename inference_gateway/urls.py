6f0093561106594915f5ac8d6626918260c71ab0
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2024-09-06 11:05:50 -0700

    Added async app to run inference service

--- inference_gateway/urls.py
+++ inference_gateway/urls.py
@@ -1,29 +1,15 @@
-"""inference_gateway URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 import os
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from resource_server_async.api import api
 
 # Batch processing feature flag
 ENABLE_BATCHES = os.getenv("ENABLE_BATCHES", False) == 'True'
 
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls), # We do not want to expose an admin page protected by simple password
     # url(r"^$", schema_view),
     path("resource_server/", include("resource_server.urls")),
     path("dashboard/", include("dashboard.urls")),
@@ -31,6 +17,9 @@ urlpatterns = [
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # Django Ninja async URL
+    path("api/", api.urls),
 ]
 
 if ENABLE_BATCHES:
