bd5f64b05e47af004a565b96010846f1f1b57f6a
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2024-09-09 15:34:04 -0700

    Rewrote inference_gateway with Django Ninja

---
+++ resource_server_async/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from resource_server_async.views import api
+
+# Use the unique namespace or versioned API instance
+urlpatterns = [
+    path('', api.urls),  # This will serve all routes under the 'resource_server_async/' URL namespace
+]
