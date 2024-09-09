eca921998fa8dfd8291caae58600d78d42e4f5a3
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2024-09-09 15:36:03 -0700

    Added async enabler variable in settings

--- inference_gateway/urls.py
+++ inference_gateway/urls.py
@@ -1,26 +1,24 @@
-import os
-from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from resource_server_async.api import api
-
-# Batch processing feature flag
-ENABLE_BATCHES = os.getenv("ENABLE_BATCHES", False) == 'True'
-
 
 urlpatterns = [
-    #path('admin/', admin.site.urls), # We do not want to expose an admin page protected by simple password
     # url(r"^$", schema_view),
-    path("resource_server/", include("resource_server.urls")),
     path("dashboard/", include("dashboard.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
-    # Django Ninja async URL
-    path("api/", api.urls),
 ]
 
-if ENABLE_BATCHES:
+# Batch processing feature
+if settings.ENABLE_BATCHES:
     urlpatterns.append(path("bulk_inference/", include("bulk_inference.urls")))
+
+# Main resource_server inference URL
+if settings.ENABLE_ASYNC:
+    # Django Ninja for asgi
+    urlpatterns.append(path("resource_server/", include("resource_server_async.urls")))
+else:
+    # Django Rest for wsgi 
+    urlpatterns.append(path("resource_server/", include("resource_server.urls")))
