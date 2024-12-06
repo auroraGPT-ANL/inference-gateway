310a1df80b9abc285d45aa7adcd7b7acb8129604
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2024-12-06 08:05:24 -0800

    removed views.py and urls.py from sync app to just keep database models

--- resource_server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from resource_server import views
-
-# URLs to access different Django views
-urlpatterns = [
-    re_path(r"^polaris/(?P<framework>\w+)/v1/(?P<openai_endpoint>chat/completions|completions|embeddings)/?$", views.Polaris.as_view(), name="polaris"),
-    re_path(r"^sophia/(?P<framework>\w+)/v1/(?P<openai_endpoint>chat/completions|completions|embeddings)/?$", views.Sophia.as_view(), name="sophia"),
-    path("list-endpoints", views.ListEndpoints.as_view(), name="list-endpoints"),
-]
