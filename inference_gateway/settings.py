a7b709d58413f3ca5cc771c8729ae65ee6bf051d
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2024-12-06 07:54:09 -0800

    temporarily added resource_server app to run with current models

--- inference_gateway/settings.py
+++ inference_gateway/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'resource_server',
     'resource_server_async',
     'drf_spectacular',
     'dashboard'
