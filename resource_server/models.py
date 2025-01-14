08dbb12ee8597a45c34c4efc9beb724e4b2ef7d3
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2025-01-14 15:26:24 -0800

    Moved and edited: batch job database model

--- resource_server/models.py
+++ resource_server/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
+import uuid
+from django.utils.timezone import now
 
 # Details of a given Globus Compute endpoint
 class Endpoint(models.Model):
@@ -119,3 +121,42 @@ class ListEndpointsLog(models.Model):
     # String function
     def __str__(self):
         return f"<{self.username} - {self.timestamp_receive} - {self.response_status}>"
+
+
+class Batch(models.Model):
+
+    # Unique UUID assigned to the batch request
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    # Who submitted the batch?
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+
+    # What did the user request?
+    endpoint = models.CharField(max_length=250)
+    input_file_id = models.CharField(max_length=250) # actually a file path
+    cluster = models.CharField(max_length=250)
+    framework = models.CharField(max_length=250)
+    metadata = models.JSONField(default=dict)
+    completion_window = models.CharField(max_length=100)
+
+    # What is the status of the batch?
+    object = models.CharField(max_length=100, default="batch")
+    errors = models.JSONField(default=dict)
+    status = models.CharField(max_length=250, default="pending")
+    output_file_id = models.CharField(max_length=250, blank=True)
+    error_file_id = models.CharField(max_length=250, blank=True)
+    created_at = models.DateTimeField(default=now)
+    in_progress_at = models.DateTimeField(null=True, blank=True)
+    expires_at = models.DateTimeField(null=True, blank=True)
+    finalizing_at = models.DateTimeField(null=True, blank=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    failed_at = models.DateTimeField(null=True, blank=True)
+    expired_at = models.DateTimeField(null=True, blank=True)
+    cancelling_at = models.DateTimeField(null=True, blank=True)
+    cancelled_at = models.DateTimeField(null=True, blank=True)
+    request_counts = models.JSONField(default=dict)
+
+    # String function
+    def __str__(self):
+        return f"Batch - <{self.username} - {self.created_at}>"
