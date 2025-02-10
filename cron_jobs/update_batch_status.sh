d28bf065a7450a804ec045679ee0b5a793110d56
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2025-02-10 09:00:33 -0800

    streamlined data command

--- cron_jobs/update_batch_status.sh
+++ cron_jobs/update_batch_status.sh
@@ -1,6 +1,6 @@
 cd /home/webportal/inference-gateway
 source .venv/bin/activate
 echo "-------------------" >> /home/webportal/inference-gateway/cron_jobs/cron_logs.txt
-echo `date '+%d/%m/%Y_%H:%M:%S'` >> /home/webportal/inference-gateway/cron_jobs/cron_logs.txt
+date '+%d/%m/%Y_%H:%M:%S' >> /home/webportal/inference-gateway/cron_jobs/cron_logs.txt
 echo "executing update_batch_status" >> /home/webportal/inference-gateway/cron_jobs/cron_logs.txt
 python3 manage.py update_batch_status
