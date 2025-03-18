6f7800143c3359679495127ea31d24f72a57dd99
Aditya Tanikanti <aditya.tanikanti@gmail.com>
2025-03-18 01:04:10 -0500

    Updated materialized view cron job refresher

--- cron_jobs/refresh_materialized_views.sh
+++ cron_jobs/refresh_materialized_views.sh
@@ -7,4 +7,12 @@ psql -U dataportaldev -d inferencegateway -c "REFRESH MATERIALIZED VIEW mv_overa
                                           REFRESH MATERIALIZED VIEW mv_model_throughput;
                                           REFRESH MATERIALIZED VIEW mv_weekly_usage;
                                           REFRESH MATERIALIZED VIEW mv_daily_usage_2_weeks;
-                                          REFRESH MATERIALIZED VIEW mv_requests_per_user;"
+                                          REFRESH MATERIALIZED VIEW mv_requests_per_user;
+                                          REFRESH MATERIALIZED VIEW mv_batch_total_jobs;
+                                          REFRESH MATERIALIZED VIEW mv_batch_successful_requests;
+                                          REFRESH MATERIALIZED VIEW mv_batch_requests_per_model;
+                                          REFRESH MATERIALIZED VIEW mv_batch_unique_users;
+                                          REFRESH MATERIALIZED VIEW mv_batch_total_tokens;
+                                          REFRESH MATERIALIZED VIEW mv_batch_avg_latency;
+                                          REFRESH MATERIALIZED VIEW mv_batch_avg_throughput;
+                                          REFRESH MATERIALIZED VIEW mv_batch_daily_usage;"
