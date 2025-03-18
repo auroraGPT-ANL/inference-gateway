f6ad6ca764dcd2bda88829ce2c2a1cc29c2ab82e
Aditya Tanikanti <aditya.tanikanti@gmail.com>
2025-03-18 10:49:57 -0500

    Removed duplicate bulk inference code

--- agpt_bulk_cli/app/batch.py
+++
@@ -1,7 +0,0 @@
-import typer
-
-batch_app = typer.Typer(name="batch", no_args_is_help=True)
-
-@batch_app.command()
-def prepare():
-    print("Under construction")
--- bulk_inference/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Batches
-
-# URLs to access different Django views
-urlpatterns = [
-    path("batches", Batches.as_view(), name="batches"),
-    path("batches/<uuid:id>/", Batches.as_view(), name="batch-detail"),
-]
