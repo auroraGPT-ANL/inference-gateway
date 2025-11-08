8012c1bd26210aa29934175657fa7f822a257a05
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2025-11-07 16:46:14 -0800

    improved check_permission function and moved endpoint-list to views.py

--- resource_server_async/clusters/cluster.py
+++ resource_server_async/clusters/cluster.py
@@ -2,10 +2,13 @@ import uuid
 from pydantic import BaseModel, Field
 from abc import ABC, abstractmethod
 from typing import List, Optional, Dict
-from resource_server_async.models import User, Endpoint
+from resource_server_async.models import User
 from utils.auth_utils import check_permission as auth_utils_check_permission
 from utils.auth_utils import CheckPermissionResponse
 
+class BaseModelWithError(BaseModel):
+    error_message: Optional[str] = Field(default=None)
+    error_code: Optional[int] = Field(default=None)
 
 class Jobs(BaseModel):
     running: List[Dict] = Field(default_factory=list)
@@ -15,10 +18,6 @@ class Jobs(BaseModel):
     private_batch_running: List[Dict] = Field(default_factory=list)
     private_batch_queued: List[Dict] = Field(default_factory=list)
     cluster_status: Dict = Field(default_factory=dict)
-
-class BaseModelWithError(BaseModel):
-    error_message: Optional[str] = Field(default=None)
-    error_code: Optional[int] = Field(default=None)
 
 class GetJobsResponse(BaseModelWithError):
     status: Optional[Jobs] = None
@@ -55,53 +54,22 @@ class BaseCluster(ABC):
         # Extract list of allowed domains
         self._allowed_domains = [d.strip() for d in self._allowed_domains.split(",") if d.strip()]
 
-
     # Check permission
     def check_permission(self, auth: User, user_group_uuids: List[str]) -> CheckPermissionResponse:
         """Verify is the user is permitted to access this endpoint."""
-        return auth_utils_check_permission(auth, user_group_uuids, self.allowed_globus_groups, self.allowed_domains)
-    
 
-    # Get endpoint list
-    async def get_endpoint_list(self, auth: User, user_group_uuids: List[str]) -> Dict:
-        """Gather list of all endpoints in the database related to this cluster."""
-
-        # For each endpoint related to this cluster ...
-        models = {}
-        async for endpoint in Endpoint.objects.filter(cluster=self.cluster_name):
-
-            # If the user is allowed to see this endpoint ...
-            response = auth_utils_check_permission(
-                auth, 
-                user_group_uuids, 
-                endpoint.allowed_globus_groups, 
-                endpoint.allowed_domains
+        # Check permission
+        response = auth_utils_check_permission(auth, user_group_uuids, self.allowed_globus_groups, self.allowed_domains)
+        if response.error_message:
+            return CheckPermissionResponse(
+                is_authorized=False,
+                error_message=response.error_message,
+                error_code=response.error_code
             )
-            if response.is_authorized:
-
-                # Collect framework and model for this endpoint
-                if endpoint.framework not in models:
-                    models[endpoint.framework] = []
-                models[endpoint.framework].append(endpoint.model)
-
-        # Initialize frameworks data structure
-        frameworks = {}
-        for fw, model_list in models.items():
-            frameworks[fw] = {
-                "models": sorted(model_list),
-                "endpoints": self.openai_endpoints
-            }
-            
-        # Build endpoint list data structure
-        cluster_endpoints = {
-            "base_url": f"/resource_server/{self.cluster_name}",
-            "frameworks": frameworks
-        }
-
-        # Return endpoint list
-        return cluster_endpoints
-
-
+        
+        # Return permission check result
+        return CheckPermissionResponse(is_authorized=response.is_authorized)
+    
     # Mandatory definitions
     # ---------------------
 
